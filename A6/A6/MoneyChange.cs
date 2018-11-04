65dfde2758f7c6cbfea3b38c1626e958b58593a1
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-04 17:51:56 +0330

    First question and half of the next one

---
+++ A6/A6/MoneyChange.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class MoneyChange: Processor
+    {
+        private static readonly int[] COINS = new int[] {1, 3, 4};
+
+        public MoneyChange(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long>) Solve);
+
+        public static long Solve(long n)
+        {
+            long[] minCoins = new long[n+1];
+            for (int i = 0; i < n+1; i++)
+                minCoins[i] = long.MaxValue;
+            minCoins[0] = 0;
+
+            for (int i = 1; i < n+1; i++)
+                for (int j = 0; j < COINS.Length; j++)
+                    if (i - COINS[j] >= 0 && COINS[j] <= n && minCoins[i - COINS[j]] < minCoins[i])
+                        minCoins[i] = minCoins[i - COINS[j]] + 1;
+            
+            return minCoins[n];
+        }
+    }
+}
---
+++ A6/A6/PrimitiveCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCommon;
+
+namespace A6
+{
+    public class PrimitiveCalculator: Processor
+    {
+        public PrimitiveCalculator(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long, long[]>)Solve);
+
+        public void MinimumSteps(long n, List<long> Steps)
+        {
+            var WhichOnes = new List<double>() { n - 1, n / 2, n / 3};
+            WhichOnes.Where(x => (int)x == x);
+            Steps.Add(Steps[(int)WhichOnes.Min()] + 1);
+        }
+
+        public long[] Solve(long n)
+        {
+            var steps = new List<long>() { 0, 0 };
+            for (int i = 2; i <= n; i++)
+                MinimumSteps(i, steps);
+            //Write your code here
+            return new long[] { 0 };
+        }
+    }
+}
