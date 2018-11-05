327848d52a8a00cdd0896411dddb9b109c809bcb
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-05 11:38:37 +0330

    First question passed WITH timeout

--- E1/Q1/Q1/FastLM.cs
+++ E1/Q1/Q1/FastLM.cs
@@ -19,8 +19,32 @@ namespace Q1
         public bool GetCount(string word, out ulong count)
         {
             count = 0;
-            //TODO
-            return false;
+            long index = BinarySearch(WordCounts, 0, WordCounts.Length - 1, word);
+
+            if (WordCounts[(int)index].Word != word)
+                return false;
+            else
+            {
+                count = WordCounts[(int)index].Count;
+                return true;
+            }
+        }
+
+        public long BinarySearch(WordCount[] words, int low, int high, string word)
+        {
+            if (high < low)
+                return low - 1;
+
+            int mid = (high + low) / 2;
+            
+            if (word == words[mid].Word)
+                return mid;
+
+            else if (String.Compare(word, words[mid].Word) < 0)
+                return BinarySearch(words, low, mid - 1, word);
+
+            else
+                return BinarySearch(words, mid + 1, high, word);
         }
     }
 }
