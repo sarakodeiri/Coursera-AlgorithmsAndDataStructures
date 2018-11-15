e468fe7128a2678b79500dd74c03e11bd6f790b2
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-15 15:06:58 +0330

    Done.

--- A7/A7/PartitioningSouvenirs.cs
+++ A7/A7/PartitioningSouvenirs.cs
@@ -17,10 +17,51 @@ namespace A7
         public long Solve(long souvenirsCount, long[] souvenirs)
         {
             long sum = souvenirs.Sum();
-            if (sum%3 != 0 || souvenirs.Length < 3 || sum / 3 < souvenirs.Max())
+            int n = souvenirs.Length;
+
+            if (sum%3 != 0 || n < 3 || sum / 3 < souvenirs.Max())
                 return 0;
 
-            return 1;
+            long g = sum / 3; //g is short for goal, meaning the sum we want each of our partitions to have.
+
+            bool[,,] allData = new bool[(int)g + 1, (int)g + 1, n];
+
+            //initializing our 3d array of data
+            for (int i = 0; i < g + 1; i++)
+                for (int j = 0; j < g + 1; j++)
+                    for (int k = 0; k < n; k++)
+                    {
+                        allData[i, 0, k] = ((i == 0) || (souvenirs[k] == i));
+                        allData[0, j, k] = ((j == 0) || (souvenirs[k] == j));
+                    }
+            
+
+            for (int i = 1; i < g + 1; i++)
+                for (int j = 1; j < g + 1; j++)
+                    for (int k = 0; k < n; k++)
+                    {
+                        int currentSouvenir = (int)souvenirs[k];
+
+                        bool canBeFilled = false;
+
+                        //get new value based on the previous one.
+                        if (k > 0)
+                        {
+                            canBeFilled = allData[i, j, k - 1];
+
+                            if ((currentSouvenir <= i && allData[i - currentSouvenir, j, k - 1])
+                                || (currentSouvenir <= j && allData[i, j - currentSouvenir, k - 1]))
+
+                                canBeFilled = true;
+                        }
+                        
+                        allData[i, j, k] = canBeFilled;
+                    }
+
+            if (allData[g, g, n - 1])
+                return 1;
+
+            return 0;
         }
     }
 }
