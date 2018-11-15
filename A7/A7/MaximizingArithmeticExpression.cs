79a53802ee90215ca158ed55bf8e6c71679469e9
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-15 12:47:58 +0330

    Only Q2 left.

--- A7/A7/MaximizingArithmeticExpression.cs
+++ A7/A7/MaximizingArithmeticExpression.cs
@@ -14,49 +14,6 @@ namespace A7
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<string, long>)Solve);
 
-        public long Solve(string expression)
-        {
-            expression.ToCharArray();
-            int n = expression.Length;
-            List<long> digits = new List<long>();
-            List<char> ops = new List<char>();
-
-            for (int i = 0; i < n; i++) //separating numbers and operators
-                if (i % 2 == 0)
-                    digits.Add(long.Parse(expression[i].ToString()));
-                else
-                    ops.Add(expression[i]);
-            
-            int m = digits.Count;
-
-            long[,] minData = new long[m + 1, m + 1];
-            long[,] maxData = new long[m + 1, m + 1];
-
-            for (int i = 0; i < m + 1; i++)
-                for (int j = 0; j < m + 1; j++)
-                {
-                    if (i == 0 || j == 0)
-                    {
-                        minData[i, j] = 0;
-                        maxData[i, j] = 0;
-                    }
-
-                    minData[i, i] = digits[i];
-                    maxData[i, i] = digits[i];
-                }
-
-            for (int s = 1; s <= m - 1; s++)
-                for (int i = 1; i <= m - s; i++)
-                {
-                    int j = i + s;
-                    var temp = MinAndMax(i, j, ops, minData, maxData);
-                    minData[i, j] = temp.Item1;
-                    maxData[i, j] = temp.Item2;
-                }
-
-            return maxData[0, m];
-        }
-
         public long BinaryOperater(long a, char op, long b)
         {
             switch (op)
@@ -90,6 +47,43 @@ namespace A7
             }
 
             return (finalMin, finalMax);
+        }
+
+        public long Solve(string expression)
+        {
+            expression.ToCharArray();
+            int n = expression.Length;
+            List<long> digits = new List<long>();
+            List<char> ops = new List<char>();
+
+            for (int i = 0; i < n; i++) //separating numbers and operators
+                if (i % 2 == 0)
+                    digits.Add(long.Parse(expression[i].ToString()));
+                else
+                    ops.Add(expression[i]);
+            
+            int m = digits.Count;
+
+            long[,] minData = new long[m, m];
+            long[,] maxData = new long[m, m];
+
+            for (int i = 0; i < m; i++)
+            {
+                minData[i, i] = digits[i];
+                maxData[i, i] = digits[i];
+            }
+
+            //Parantheses part of the pseudo code
+            for (int s = 0; s < m; s++)
+                for (int i = 0; i < m - s - 1; i++)
+                {
+                    int j = i + s + 1;
+                    var temp = MinAndMax(i, j, ops, minData, maxData);
+                    minData[i, j] = temp.Item1;
+                    maxData[i, j] = temp.Item2;
+                }
+
+            return maxData[0, m - 1];
         }
     }
 }
