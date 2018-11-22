5d500df4b3d0d5df4af0ebd2690ba4c4cf5a5857
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-22 14:32:27 +0330

    Q2 passed.

--- A8/A8/Program.cs
+++ A8/A8/Program.cs
@@ -10,7 +10,7 @@ namespace A7
     {
         static void Main(string[] args)
         {
-
+           
         }
     }
 }
--- A8/A8/TreeHeight.cs
+++ A8/A8/TreeHeight.cs
@@ -14,9 +14,53 @@ namespace A8
         public override string Process(string inStr) =>
             TestTools.Process(inStr, (Func<long, long[], long>)Solve);
 
-        public long Solve(long nodeCount, long[] tree)
+
+        public class Node
         {
-            return 0;
+            public List<Node> children = new List<Node>();
+            public int depth;
+            public Node() { }
         }
+
+        public class Tree
+        {
+            public Node root;
+            public List<Node> nodes = new List<Node>();
+            public Tree (long[] tree)
+            {
+                for (int i = 0; i < tree.Length; i++)
+                    nodes.Add(new Node());
+
+                for (int i = 0; i < tree.Length; i++)
+                    if (tree[i] != -1)
+                        nodes[(int)tree[i]].children.Add(nodes[i]);
+            
+                    else
+                        root = nodes[i];
+            }
+        
+        }
+        
+        public long Solve(long nodeCount, long[] treeList)
+        {
+            Tree tree = new Tree(treeList);
+            int height = 1;
+            Queue<Node> queue = new Queue<Node>();
+            queue.Enqueue(tree.root);
+
+            do
+            {
+                Node currentNode = queue.Dequeue(); 
+                for (int i = 0; i < currentNode.children.Count; i++)
+                {
+                    queue.Enqueue(currentNode.children[i]);
+                    currentNode.children[i].depth = currentNode.depth + 1;
+                    height = Math.Max(height, currentNode.depth + 1);
+                }
+            } while (queue.Count != 0);
+
+            return height + 1;
+        }
+
     }
 }
