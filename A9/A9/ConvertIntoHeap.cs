7c69e3aa4c45e0074f12996c47a122ca2f7190cb
Sara Kodeiri <skaordaeiri@outlook.com>
2018-11-30 15:50:26 +0330

    Q1.

---
+++ A9/A9/ConvertIntoHeap.cs
@@ -0,0 +1,59 @@
+﻿using TestCommon;
+using System;
+using System.Collections.Generic;
+
+namespace A9
+{
+    public class ConvertIntoHeap : Processor
+    {
+        public ConvertIntoHeap(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[], Tuple<long, long>[]>)Solve);
+
+        public Tuple<long, long>[] Solve(
+            long[] array)
+        {
+            List<Tuple<long, long>> swapsList = new List<Tuple<long, long>>();
+            int n = array.Length;
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int index;
+                if (i % 2 == 0)
+                    if (i - 1 >= 0)
+                        index = array[i] > array[i - 1] ? i - 1 : i;
+                    else
+                        index = i;
+                else
+                    if (i + 1 <= n - 1)
+                        index = array[i] > array[i + 1] ? i + 1 : i;
+                    else
+                        index = i;
+
+                int parentIndex = (i + 1) / 2 - 1;
+
+                while (parentIndex <= n - 1 && index <= n - 1)
+                {
+                    if (array[parentIndex] > array[index])
+                    {
+                        swapsList.Add(new Tuple<long, long>(parentIndex, index));
+                        (array[index], array[parentIndex]) = (array[parentIndex], array[index]);
+                        parentIndex = index;
+                        
+                        if (index * 2 + 2 <= n - 1 && array[index * 2 + 1] > array[index * 2 + 2])
+                            index = index * 2 + 2;
+                        else
+                            index = index * 2 + 1;
+                    }
+
+                    else
+                        break;
+                }
+            }
+
+            return swapsList.ToArray();
+        }
+
+    }
+}
