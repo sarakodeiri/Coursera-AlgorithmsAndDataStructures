5a5ce927174f69d6b61902b951bcc90f60aab94b
Sara Kodeiri <skaordaeiri@outlook.com>
2018-12-20 19:27:40 +0330

    Q1 passed

---
+++ A11/A11/BinaryTreeTraversals.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCommon;
+
+namespace A11
+{
+    public class BinaryTreeTraversals : Processor
+    {
+        public BinaryTreeTraversals(string testDataName) : base(testDataName) { }
+
+        public override string Process(string inStr) =>
+            TestTools.Process(inStr, (Func<long[][], long[][]>)Solve);
+
+        List<long> InOrderResult;
+        List<long> PreOrderResult;
+        List<long> PostOrderResult;
+        
+        private void InOrder(Node root)
+        {
+            InOrderResult = new List<long>();
+            Stack<Node> nodeStack = new Stack<Node>();
+            Node currentNode = root;
+            if (root == null)
+                return;
+            
+            while (nodeStack.Count > 0 || currentNode != null)
+            {
+                while (currentNode != null)
+                {
+                    nodeStack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+
+                currentNode = nodeStack.Pop();
+                InOrderResult.Add(currentNode.key);
+                currentNode = currentNode.right;
+            }
+        }
+
+        private void PreOrder(Node root)
+        {
+            PreOrderResult = new List<long>();
+            Stack<Node> nodeStack = new Stack<Node>();
+            nodeStack.Push(root);
+            if (root == null)
+                return;
+
+            while (nodeStack.Count > 0)
+            {
+
+                Node peeked = nodeStack.Peek();
+                PreOrderResult.Add(peeked.key);
+                nodeStack.Pop();
+                
+                if (peeked.right != null)
+                    nodeStack.Push(peeked.right);
+                
+                if (peeked.left != null)
+                    nodeStack.Push(peeked.left);
+            }
+        }
+
+        private void PostOrder(Node root)
+        {
+            Stack<Node> nodeStack = new Stack<Node>();
+            PostOrderResult = new List<long>();
+            nodeStack.Push(root);
+            if (root == null)
+                return;
+           
+            while (nodeStack.Count > 0)
+            {
+                Node peeked = nodeStack.Peek();
+                
+                if (peeked.right == root || peeked.left == root || peeked.left == null && peeked.right == null)
+                {
+                    nodeStack.Pop();
+                    PostOrderResult.Add(peeked.key);
+                    root = peeked;
+                }
+
+                else
+                {
+                    if (peeked.right != null)
+                        nodeStack.Push(peeked.right);
+
+                    if (peeked.left != null)
+                        nodeStack.Push(peeked.left);
+                }
+            }
+        }
+
+        public long[][] Solve(long[][] nodes)
+        {
+            Tree tree = new Tree(nodes);
+            InOrder(tree.root);
+            PreOrder(tree.root);
+            PostOrder(tree.root);
+
+            long[][] finalResult = new long[3][];
+            finalResult[0] = InOrderResult.ToArray();
+            finalResult[1] = PreOrderResult.ToArray();
+            finalResult[2] = PostOrderResult.ToArray();
+
+            return finalResult;
+        }
+
+    }
+}
---
+++ A11/A11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long[][] nodes = new long[5][];
+            nodes[0] = new long[] {14, 3, 4};
+            nodes[1] = new long[] {5, -1, -1};
+            nodes[2] = new long[] {9, -1, 1};
+            nodes[3] = new long[] {17, -1, -1};
+            nodes[4] = new long[] {6, -1, 2};
+
+
+
+            //BinaryTreeTraversals blah = new BinaryTreeTraversals("TD1");
+            //BinaryTreeTraversals.Tree tree = new BinaryTreeTraversals.Tree(nodes);
+            //blah.Solve(nodes);
+        }
+    }
+}
