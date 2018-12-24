5305acd5f76769c20f2989fbdf7f7360bfc8d1cd
Sara Kodeiri <skaordaeiri@outlook.com>
2018-12-24 14:55:07 +0330

    Probably final commit

--- E2/E2/Q3BloomFilter.cs
+++ E2/E2/Q3BloomFilter.cs
@@ -8,25 +8,24 @@ namespace E2
     {
         BitArray Filter; 
         int[] RandInts;
-        int Count;
         int FilterSize;
+        int HashFNCount;
         Func<string, int>[] HashFunctions;
 
 
         public Q3BloomFilter(int filterSize, int hashFnCount) //ToDo (First few lines)
         {
-            Count = hashFnCount;
-            FilterSize = filterSize;
-            Random rnd = new Random();
             Filter = new BitArray(filterSize);
-            RandInts = new int[Count];
+            HashFNCount = hashFnCount;
+            RandInts = new int[HashFNCount];
+            FilterSize = filterSize;
 
-            for (int i = 0; i < Count; i++)
+            Random rnd = new Random();
+            for (int i = 0; i < HashFNCount; i++)
                 RandInts[i] = rnd.Next();
 
-            HashFunctions = new Func<string, int>[Count];
-
-            for (int i = 0; i < Count; i++)
+            HashFunctions = new Func<string, int>[HashFNCount];
+            for (int i = 0; i < HashFNCount; i++)
             {
                 int x = RandInts[i];
                 HashFunctions[i] = str => PolyHash(str, x); //takes string, converts it into an int (the hash).
@@ -41,21 +40,26 @@ namespace E2
         public int PolyHash(string str, int x) //Copied from A10
         {
             int hash = 0;
+            int p = 1000000007;
             for (int i = str.Length - 1; i >= 0; i--)
-                hash = (hash * x + str[i]) % 1000000007;
+                hash = (hash * x + str[i]) % p;
 
-            return hash;
+            return (hash%p + 1000*p) % p;
         }
 
         public void Add(string str) //ToDo
         {
-            for (int i = 0; i < Count; i++)
-                Filter[HashFunctions[i](str) % FilterSize] = true;
+            for (int i = 0; i < HashFNCount; i++)
+            {
+                int hashed = (HashFunctions[i](str))*(Filter.Length) % Filter.Length;
+                Filter[hashed] = true;
+
+            }
         }
 
         public bool Test(string str) //ToDo
         {
-            for (int i = 0; i < Count; i++)
+            for (int i = 0; i < HashFNCount; i++)
                 if (!Filter[HashFunctions[i](str) % FilterSize])
                     return false;
             return true;
