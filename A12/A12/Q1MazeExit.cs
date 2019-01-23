4acbd03f1e1c75f8a326fa75de5f2e1666d998d7
Sara Kodeiri <skaordaeiri@outlook.com>
2019-01-23 17:37:13 +0330

    Q1 done

--- A12/A12/Q1MazeExit.cs
+++ A12/A12/Q1MazeExit.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TestCommon;
 
 namespace A12
@@ -13,7 +14,36 @@ namespace A12
         public long Solve(long nodeCount, long[][] edges, long StartNode, long EndNode)
         {
             // Your code here
+            List<long>[] adjacencyList = new List<long>[nodeCount];
+            for (int i = 0; i < adjacencyList.Length; i++)
+                adjacencyList[i] = new List<long>();
+
+            bool[] visited = new bool[nodeCount];
+            for (int i = 0; i < visited.Length; i++)
+                visited[i] = false;
+
+
+            for (int i=0; i<edges.Length; i++)
+            {
+                long first = edges[i][0] - 1;
+                long second = edges[i][1] - 1;
+                adjacencyList[first].Add(second);
+                adjacencyList[second].Add(first);
+            }
+
+            Explore(adjacencyList, StartNode - 1, visited);
+
+            if (visited[EndNode - 1])
+                return 1;
             return 0;
-        }    
+        }
+        
+        private void Explore(List<long>[] adj, long Start, bool[] visited)
+        {
+            visited[Start] = true;
+            foreach (var v in adj[Start])
+                if (!visited[v])
+                    Explore(adj, v, visited);
+        }
      }
 }
